1864add31660b1d9054369c2f2ac4ef06e0ba63c
raissacst <00001106266729sp@al.educacao.sp.gov.br>
2024-10-13 19:49:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,136 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+
+
+criaCartao(
+    'geografia',
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest'
+)
+criaCartao(
+    'anatomia',
+    'Quantos ossos temos no nosso corpo?',
+    ' 206'
+)
+criaCartao(
+    'fisica',
+    'A que temperatura a água ferve?',
+    '100 ºC'
+)
+criaCartao(
+    'astronomia',
+    'Qual o maior planeta do sistema solar?',
+    'Júpiter'
+)
+criaCartao(
+    'geografia',
+    'Quantos continentes existem?',
+    '6'
+)
+
+
+criaCartao(
+    'entreterimento',
+    'Kryptonita é a fraqueza de qual super-herói?',
+    'Super-Homem'
+)
+criaCartao(
+    'quimica',
+    'Qual o nome popular do cloreto de sódio?',
+    'Sal de cozinha'
+)
+criaCartao(
+    'biologia',
+    'O que os pandas comem?',
+    'bambu'
+)
+criaCartao(
+    'Matematica',
+    'Qual a fórmula para calcular a área de um triângulo?',
+    'base x altura / 2'
+)
+
+criaCartao(
+    'Educaçao fisica',
+    ' Qual o número mínimo de jogadores em cada time numa partida de futebol?',
+    '7'
+)
+criaCartao(
+    'Matematica',
+    'Qual é o nome da figura geométrica que tem cinco lados?',
+    'pentagono'
+)
+
+
+criaCartao(
+    'Educaçao fisica',
+    '  Quantas champions league o Cristiano Ronaldo têm? ',
+    '5'
+)
+
+criaCartao(
+    'Educaçao fisica',
+    ' Depois do futebol, qual o esporte mais popular no Brasil? ',
+    'Vôlei.'
+)
+
+criaCartao(
+    'Ingles',
+    ' O que a palavra legend significa em português? ',
+    'lenda'
+)
+criaCartao(
+    'Educaçao fisica',
+    'Quantos jogadores compõem uma equipe de voleibol em quadra durante uma partida?? ',
+    '6'
+)
+criaCartao(
+    'Educaçao fisica',
+    ' Quem ganhou a Copa do Mundo de 2010?',
+    'Espanha'
+
+)
+criaCartao(
+    'Educaçao fisica',
+    ' Qual time ganhou mais Champions League na história?',
+    'Real Madrid'  
+)
+criaCartao(
+    'biologia',
+    ' Quantos cromossomo ha em  uma pessoa normal?',
+    '46'  
+)
+criaCartao(
+    'Historia',
+    ' Quanto tempo durou a guerra dos 100 anos?',
+    '116 anos'  
+)
+criaCartao(
+    'Matematica',
+    ' Qual é a fórmula para calcular a área de um círculo?',
+    'A = π r²'  
+)
+
+
